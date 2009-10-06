c5d2ed692ff60a517025b278da5d521d993ee475
Sidorenko Vladimir <yoyavova@gmail.com>
2009-10-06 23:14:31 +0300

    bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="app_media",
-           version="0.1",
+           version="0.1.1",
            description="Django application to handle application media",
            author="Vladimir Sidorenko",
            author_email="yoyavova@gmail.com",
